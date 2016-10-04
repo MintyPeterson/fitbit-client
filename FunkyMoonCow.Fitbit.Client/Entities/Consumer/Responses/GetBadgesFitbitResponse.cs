e468e456c677e32cb88988eb600e6cfea227fc97
Tom Cook <tom.cook@ymail.com>
2016-10-04 09:47:02 +0100

    Added undocumented GetBadges properties.

--- FunkyMoonCow.Fitbit.Client/Entities/Consumer/Responses/GetBadgesFitbitResponse.cs
+++ FunkyMoonCow.Fitbit.Client/Entities/Consumer/Responses/GetBadgesFitbitResponse.cs
@@ -71,11 +71,27 @@ namespace FunkyMoonCow.Fitbit
           badges.Add(
             new GetBadgesFitbitResponseBadge
             {
+              GradientEndColour = result.badgeGradientEndColor,
+              GradientStartColour = result.badgeGradientStartColor,
               BadgeType = result.badgeType,
+              Category = result.category,
               DateMostRecentlyAchieved = result.dateTime,
+              Description = result.description,
+              CongratulationsMessage = result.earnedMessage,
+              EncodedId = result.encodedId,
+              MediumImageUrl = result.image100px,
+              LargeImageUrl = result.image125px,
+              ExtraLargeImageUrl = result.image300px,
+              ExtraSmallImageUrl = result.image50px,
+              SmallImageUrl = result.image75px,
+              LongDescription = result.marketingDescription,
+              MotivationalMessage = result.mobileDescription,
+              Name = result.name,
+              JumboImageUrl = result.shareImage640px,
+              SocialMediaMessage = result.shareMessage,
+              ShortDescription = result.shortDescription,
+              ShortName = result.shortName,
               NumberOfTimesAchieved = result.timesAchieved,
-              LargeImageUrl = result.image75px,
-              SmallImageUrl = result.image50px,
               Unit = result.unit,
               Value = result.value
             }
@@ -103,14 +119,34 @@ namespace FunkyMoonCow.Fitbit
     public DateTime DateMostRecentlyAchieved { get; set; }
 
     /// <summary>
-    /// Gets or sets the small image URL.
+    /// Gets or sets the extra small (50px) image URL.
+    /// </summary>
+    public string ExtraSmallImageUrl { get; set; }
+
+    /// <summary>
+    /// Gets or sets the small (75px) image URL.
     /// </summary>
     public string SmallImageUrl { get; set; }
 
     /// <summary>
-    /// Gets or sets the large image URL.
+    /// Gets or sets the medium (100px) image URL.
+    /// </summary>
+    public string MediumImageUrl { get; set; }
+
+    /// <summary>
+    /// Gets or sets the large (125px) image URL.
     /// </summary>
     public string LargeImageUrl { get; set; }
+
+    /// <summary>
+    /// Gets or sets the extra large (300px) image URL.
+    /// </summary>
+    public string ExtraLargeImageUrl { get; set; }
+
+    /// <summary>
+    /// Gets or sets the jumbo (640px) image URL.
+    /// </summary>
+    public string JumboImageUrl { get; set; }
 
     /// <summary>
     /// Gets or sets the number of times the badge has been achived.
@@ -126,6 +162,66 @@ namespace FunkyMoonCow.Fitbit
     /// Get or sets the value.
     /// </summary>
     public float Value { get; set; }
+
+    /// <summary>
+    /// Gets or sets the gradient end colour.
+    /// </summary>
+    public string GradientEndColour { get; set; }
+
+    /// <summary>
+    /// Gets or sets the gradient start colour.
+    /// </summary>
+    public string GradientStartColour { get; set; }
+
+    /// <summary>
+    /// Gets or sets the category.
+    /// </summary>
+    public string Category { get; set; }
+
+    /// <summary>
+    /// Gets or sets the description.
+    /// </summary>
+    public string Description { get; set; }
+
+    /// <summary>
+    /// Gets or sets the marketing description.
+    /// </summary>
+    public string LongDescription { get; set; }
+
+    /// <summary>
+    /// Gets or sets the short description.
+    /// </summary>
+    public string ShortDescription { get; set; }
+
+    /// <summary>
+    /// Gets or sets the congratulations message.
+    /// </summary>
+    public string CongratulationsMessage { get; set; }
+
+    /// <summary>
+    /// Gets or sets the motivational message. 
+    /// </summary>
+    public string MotivationalMessage { get; set; }
+
+    /// <summary>
+    /// Gets or sets the encoded identification number.
+    /// </summary>
+    public string EncodedId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the name.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the short name.
+    /// </summary>
+    public string ShortName { get; set; }
+
+    /// <summary>
+    /// Gets or sets the social media message.
+    /// </summary>
+    public string SocialMediaMessage { get; set; }
 
     /// <summary>
     /// Initialises a new instance of the <see cref="GetBadgesFitbitResponseBadge"/> class.
