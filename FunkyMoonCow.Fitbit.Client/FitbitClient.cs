8f664acedb35f2b9ef71aaf7f434475f994b6c3a
Tom Cook <tom.cook@ymail.com>
2016-11-20 11:33:28 +0000

    Tidied usings.

--- FunkyMoonCow.Fitbit.Client/FitbitClient.cs
+++ FunkyMoonCow.Fitbit.Client/FitbitClient.cs
@@ -3,7 +3,6 @@ using Newtonsoft.Json.Schema;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
